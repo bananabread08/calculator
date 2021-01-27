6e7de057e1d59041e22ba2f0f518e46a0b771072
bananabread08 <rmkfermin@gmail.com>
2021-01-27 12:27:15 +0800

    initialize all files (HTML, CSS, JS), added the basic functions for JS file

---
+++ scripts/calc.js
@@ -0,0 +1,12 @@
+const add = (x,y) => x + y;
+
+const subtract = (x,y) => x - y;    
+
+const multiply = (x,y) => x * y;  
+
+const divide = (x,y) => x / y;
+    
+function operate(){
+    let x = prompt("First Num:");
+    let y = prompt("Second Num:");
+}
