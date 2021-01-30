4dcd7c8cda2c9c6a730b7aef05543f30db643d37
bananabread08 <rmkfermin@gmail.com>
2021-01-30 23:44:59 +0800

    modified code to correctly display the numbers/equation. reorganized some functions to make code more readable. basic calculator is working as intended.

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,55 +1,80 @@
 const add = (x,y) => x + y;
 
-const subtract = (x,y) => x - y;    
+const subtract = (x,y) => x - y;
 
-const multiply = (x,y) => x * y;  
+const multiply = (x,y) => x * y;
 
 const divide = (x,y) => {
     return y === 0 ? "ERROR" : x / y;  
 }
     
-function operate(str){
-    let arr = Array.from(str);
-    console.log(arr);
-    const operator = arr.filter(str => str === "+" || str === "-" || str === "*" || str === "/");
-    console.log(operator);
-    let numbers = str.split(operator);
-    console.log(numbers);
+function operate(a,b, operator){
     switch (operator){
         case "+":
-            add(numbers[0],numbers[1]);
-            break;
+            return add(a,b);
         case "-":
-            subtract(numbers[0],numbers[1]);
-            break;
+            return subtract(a,b);
         case "*":
-            multiply(numbers[0],numbers[1]);
-            break;
+            return multiply(a,b);
         case "*":
-            divide(numbers[0],numbers[1]);
-            break;
+            return divide(a,b);
+    }
+}
+
+function isOperator(x){
+    return operatorArr.includes(x);
+}
+
+function splitEquation(eqnStr){
+    let eqnArr = Array.from(eqnStr);
+    let operateVal = eqnArr.filter(element => {
+        if (isOperator(element)) return element;
+    });
+    operateVal = operateVal.toString();
+    let numbers = eqnStr.split(operateVal);
+    eqnStr = operate(parseInt(numbers[0]), parseInt(numbers[1]), operateVal);
+    return eqnStr;
+}
+
+function getInput(e){ //checks user input and evaluates the equation upon clicking "="
+    if(equation === "" && isOperator(e.target.value)){ 
+        display.textContent = "";
+        result.textContent = "";
+    }
+
+    else if(e.target.value === "="){
+        equation = splitEquation(equation);
+        display.textContent = "";
+        result.textContent = `${equation}`;
+    }
+
+    else if(isOperator(e.target.value)){
+        display.textContent = "";
+        result.textContent = `${equation}`;
+        display.textContent = `${e.target.value}`;
+        equation = equation + e.target.value;
+    }
+
+    else if(e.target.value === "clearall"){
+        equation = "";
+        display.textContent = "";
+        result.textContent = "";
+    }
+
+    else{
+        display.textContent = "";
+        equation = equation + e.target.value;
+        display.textContent = `${equation}`;
     }
 }
 
 let equation = "";
+const operatorArr = ["+", "-", "*", "/"];
 const key = document.querySelectorAll("button");
-const operators = document.querySelectorAll("#operator");
 const display = document.getElementById("display");
 const result = document.getElementById("result"); 
 
 
-
-key.forEach(button => {
-    button.addEventListener("click", e => {
-        if(e.target.value === "="){
-            display.textContent = "";
-            result.textContent = operate(equation);
-        }
-
-        else{
-            equation = equation + e.target.value;
-            console.log(equation);
-            display.textContent = `${equation}`;
-        }
-    });
+key.forEach(button => { //click events on every button
+    button.addEventListener("click", getInput);
 });
