a82d24898a0bc0b54ed457abc5fc71b6c12821c5
bananabread08 <rmkfermin@gmail.com>
2021-02-01 18:46:00 +0800

    added functionality to calculate 2 numbers at a time using = or another operator, updated css styling, added a footer

--- scripts/calc.js
+++ scripts/calc.js
@@ -25,6 +25,11 @@ function isOperator(x){
     return operatorArr.includes(x);
 }
 
+function resetDisplay(){
+    display.textContent = "";
+    result.textContent = "";
+}
+
 function splitEquation(eqnStr){
     let eqnArr = Array.from(eqnStr);
     let operateVal = eqnArr.filter(element => {
@@ -32,35 +37,47 @@ function splitEquation(eqnStr){
     });
     operateVal = operateVal.toString();
     let numbers = eqnStr.split(operateVal);
-    eqnStr = operate(parseInt(numbers[0]), parseInt(numbers[1]), operateVal);
+    eqnStr = operate(parseFloat(numbers[0]), parseFloat(numbers[1]), operateVal);
     return eqnStr;
 }
 
 function getInput(e){ //checks user input and evaluates the equation upon clicking "="
     if(equation === "" && isOperator(e.target.value)){ 
-        display.textContent = "";
-        result.textContent = "";
+        resetDisplay();
     }
-
-    else if(e.target.value === "="){
-        equation = splitEquation(equation);
-        display.textContent = "";
-        result.textContent = `${equation}`;
+    else if(e.target.value === "="){ //user inputs equals
+        if(equation === ""){ //no initial value, reset
+            resetDisplay();
+        }
+        else{ //if values are present, calculate
+            equation = splitEquation(equation);
+            display.textContent = "";
+            result.textContent = `${equation}`;
+        }
     }
+    else if(isOperator(e.target.value)){ //user inputs operator
+        let newEqn = Array.from(equation);
+        if((newEqn.includes("+") || newEqn.includes("/") || newEqn.includes("*") || newEqn.includes("-")) && (numArr.includes(equation.slice(-1)))){ //user inputs operator after an equation of 2 numbers
+            equation = splitEquation(equation);
+            display.textContent = `${equation}${e.target.value}`;
+            result.textContent = `${equation}`;
+            equation = equation + e.target.value; 
+        }
 
-    else if(isOperator(e.target.value)){
-        display.textContent = "";
-        result.textContent = `${equation}`;
-        display.textContent = `${e.target.value}`;
-        equation = equation + e.target.value;
+        else{ //
+            result.textContent = `${equation}`;
+            display.textContent = `${equation}${e.target.value}`;
+            equation = equation + e.target.value;
+        }
     }
-
     else if(e.target.value === "clearall"){
         equation = "";
-        display.textContent = "";
-        result.textContent = "";
+        resetDisplay();
     }
-
+    else if(e.target.value === "delete"){
+        equation = equation.substr(0, equation.length -1);
+        display.textContent = `${equation}`;
+    }
     else{
         display.textContent = "";
         equation = equation + e.target.value;
@@ -70,10 +87,10 @@ function getInput(e){ //checks user input and evaluates the equation upon clicki
 
 let equation = "";
 const operatorArr = ["+", "-", "*", "/"];
+const numArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const key = document.querySelectorAll("button");
 const display = document.getElementById("display");
 const result = document.getElementById("result"); 
-
 
 key.forEach(button => { //click events on every button
     button.addEventListener("click", getInput);
