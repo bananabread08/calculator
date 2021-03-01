cd7eb2656979effb93c22b96a6b82feab0a99271
bananabread08 <rmkfermin@gmail.com>
2021-03-01 17:20:05 +0800

    fixed syntax using eslint

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,97 +1,89 @@
-const add = (x,y) => x + y;
+let equation = '';
+const operatorArr = ['+', '-', '*', '/'];
+const numArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+const key = document.querySelectorAll('button');
+const display = document.getElementById('display');
+const result = document.getElementById('result');
 
-const subtract = (x,y) => x - y;
+const add = (x, y) => x + y;
 
-const multiply = (x,y) => x * y;
+const subtract = (x, y) => x - y;
 
-const divide = (x,y) => {
-    return y === 0 ? "ERROR" : x / y;  
-}
-    
-function operate(a,b, operator){
-    switch (operator){
-        case "+":
-            return add(a,b);
-        case "-":
-            return subtract(a,b);
-        case "*":
-            return multiply(a,b);
-        case "/":
-            return divide(a,b);
-    }
-}
+const multiply = (x, y) => x * y;
 
-function isOperator(x){
-    return operatorArr.includes(x);
-}
+const divide = (x, y) => (y === 0 ? 'ERROR' : x / y);
 
-function resetDisplay(){
-    display.textContent = "";
-    result.textContent = "";
+// eslint-disable-next-line consistent-return
+function operate(a, b, operator) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    //  no default
+  }
 }
 
-function splitEquation(eqnStr){
-    let eqnArr = Array.from(eqnStr);
-    let operateVal = eqnArr.filter(element => {
-        if (isOperator(element)) return element;
-    });
-    operateVal = operateVal.toString();
-    let numbers = eqnStr.split(operateVal);
-    eqnStr = operate(parseFloat(numbers[0]), parseFloat(numbers[1]), operateVal);
-    return eqnStr;
+function isOperator(x) {
+  return operatorArr.includes(x);
 }
 
-function getInput(e){ //checks user input and evaluates the equation upon clicking "="
-    if(equation === "" && isOperator(e.target.value)){ 
-        resetDisplay();
-    }
-    else if(e.target.value === "="){ //user inputs equals
-        if(equation === ""){ //no initial value, reset
-            resetDisplay();
-        }
-        else{ //if values are present, calculate
-            equation = splitEquation(equation);
-            display.textContent = "";
-            result.textContent = `${equation}`;
-        }
-    }
-    else if(isOperator(e.target.value)){ //user inputs operator
-        let newEqn = Array.from(equation);
-        if((newEqn.includes("+") || newEqn.includes("/") || newEqn.includes("*") || newEqn.includes("-")) && (numArr.includes(equation.slice(-1)))){ //user inputs operator after an equation of 2 numbers
-            equation = splitEquation(equation);
-            display.textContent = `${equation}${e.target.value}`;
-            result.textContent = `${equation}`;
-            equation = equation + e.target.value; 
-        }
-
-        else{ //
-            result.textContent = `${equation}`;
-            display.textContent = `${equation}${e.target.value}`;
-            equation = equation + e.target.value;
-        }
-    }
-    else if(e.target.value === "clearall"){
-        equation = "";
-        resetDisplay();
-    }
-    else if(e.target.value === "delete"){
-        equation = equation.substr(0, equation.length -1);
-        display.textContent = `${equation}`;
-    }
-    else{
-        display.textContent = "";
-        equation = equation + e.target.value;
-        display.textContent = `${equation}`;
-    }
+function resetDisplay() {
+  display.textContent = '';
+  result.textContent = '';
 }
 
-let equation = "";
-const operatorArr = ["+", "-", "*", "/"];
-const numArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-const key = document.querySelectorAll("button");
-const display = document.getElementById("display");
-const result = document.getElementById("result"); 
+function splitEquation(eqnStr) {
+  const eqnArr = Array.from(eqnStr);
+  let operateVal = eqnArr.filter((element) => {
+    if (isOperator(element)) return element;
+  });
+  operateVal = operateVal.toString();
+  const numbers = eqnStr.split(operateVal);
+  const newEqnStr = operate(parseFloat(numbers[0]), parseFloat(numbers[1]), operateVal);
+  return newEqnStr;
+}
 
-key.forEach(button => { //click events on every button
-    button.addEventListener("click", getInput);
+function getInput(e) { // checks user input and evaluates the equation upon clicking "="
+  if (equation === '' && isOperator(e.target.value)) {
+    resetDisplay();
+  } else if (e.target.value === '=') { // user inputs equals
+    if (equation === '') { // no initial value, reset
+      resetDisplay();
+    } else { // if values are present, calculate
+      equation = splitEquation(equation);
+      display.textContent = '';
+      result.textContent = `${equation}`;
+    }
+  } else if (isOperator(e.target.value)) { // user inputs operator
+    const newEqn = Array.from(equation);
+    if ((newEqn.includes('+') || newEqn.includes('/') || newEqn.includes('*') || newEqn.includes('-')) && (numArr.includes(equation.slice(-1)))) { // user inputs operator after an equation of 2 numbers
+      equation = splitEquation(equation);
+      display.textContent = `${equation}${e.target.value}`;
+      result.textContent = `${equation}`;
+      equation += e.target.value;
+    } else { //
+      result.textContent = `${equation}`;
+      display.textContent = `${equation}${e.target.value}`;
+      equation += e.target.value;
+    }
+  } else if (e.target.value === 'clearall') {
+    equation = '';
+    resetDisplay();
+  } else if (e.target.value === 'delete') {
+    equation = equation.substr(0, equation.length - 1);
+    display.textContent = `${equation}`;
+  } else {
+    display.textContent = '';
+    equation += e.target.value;
+    display.textContent = `${equation}`;
+  }
+}
+
+key.forEach((button) => { // click events on every button
+  button.addEventListener('click', getInput);
 });
